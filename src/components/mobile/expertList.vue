7b45465421cbfa8db8d8c6e58508c6174837f107
home-win <18321482348@163.com>
2017-07-31 02:17:11 +0800

    bugs

--- src/components/mobile/expertList.vue
+++ src/components/mobile/expertList.vue
@@ -49,27 +49,29 @@
               :pullup-config="list.pullUpConfig" @on-pulldown-loading="onRefresh" @on-pullup-loading="onLoadMore" ref="scroller">
       <ul class="data-list expert-list">
         <li class="data-item expert-list-item" v-for="(data,index) in list.dataList">
-          <div class="expert-top">
-            <img class="user-img" src="/static/img/user-default.png">
-            <div class="user-info">
-              <h1>{{data.nickName}}</h1>
-              <i class="icon-location"></i><span>{{data.cityName}}</span>
-              <i class="icon-time margin-left-5"></i><span>{{data.workExperience}}</span>
-              <img src="static/img/icon-job.png" class="img-icon margin-left-5"></img><span>{{data.industryName}}</span>
+          <router-link :to="'/expertDetail/'+data.id">
+            <div class="expert-top">
+              <img class="user-img" src="/static/img/user-default.png">
+              <div class="user-info">
+                <h1>{{data.nickName}}</h1>
+                <i class="icon-location"></i><span>{{data.cityName}}</span>
+                <i class="icon-time margin-left-5"></i><span>{{data.workExperience}}</span>
+                <img src="static/img/icon-job.png" class="img-icon margin-left-5"></img><span>{{data.industryName}}</span>
+              </div>
             </div>
-          </div>
-          <div class="expert-middle">
-            <p>{{data.selfIntroduction}} </p>
-            <ul class="gray-border-icons margin-top-10 margin-bottom-10">
-              <li v-for="item in data.jobTitle.split(',')" v-if="data.jobTitle">{{item}}</li>
-            </ul>
-            <span class="fc-red float-right">日薪/{{data.dailyWage}}</span>
-          </div>
-          <div class="expert-bottom">
-            <span class="fc-red fs-xl"><i class="icon-talk"></i>查看Ta的联系方式</span><span
-            class="fc-gray-l">（9元预约，会员免费）</span><br/>
-            <div style="line-height: 30px;">服装展会、T台秀方案设计与执行<i class="icon-arrow-right float-right"></i></div>
-          </div>
+            <div class="expert-middle">
+              <p>{{data.selfIntroduction}} </p>
+              <ul class="gray-border-icons margin-top-10 margin-bottom-10">
+                <li v-for="item in data.jobTitle.split(',')" v-if="data.jobTitle">{{item}}</li>
+              </ul>
+              <span class="fc-red float-right">日薪/{{data.dailyWage}}</span>
+            </div>
+            <div class="expert-bottom">
+              <span class="fc-red fs-xl"><i class="icon-talk"></i>查看Ta的联系方式</span><span
+              class="fc-gray-l">（9元预约，会员免费）</span><br/>
+              <div style="line-height: 30px;">服装展会、T台秀方案设计与执行<i class="icon-arrow-right float-right"></i></div>
+            </div>
+          </router-link>
         </li>
       </ul>
     </scroller>
